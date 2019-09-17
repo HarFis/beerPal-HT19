ced7a24d1f6c2c3766ffcff0de547328735e88f2
HarFis <gusfisha@student.gu.se>
2019-09-17 15:47:35 +0200

    Kill the camel in app.js - super huge test included
    Test run successufully

--- server/app.js
+++ server/app.js
@@ -11,7 +11,6 @@ var breweriesController = require('./controllers/breweries');
 var usersController = require('./controllers/users');
 var locationsController = require('./controllers/locations');
 var reviewsController = require('./controllers/reviews');
-var camelsController = require('./controllers/camels');
 var postsController = require('./controllers/posts');
 
 
@@ -61,9 +60,6 @@ app.use('/api/locations', locationsController);
 
 // Route for reviews
 app.use('/api/reviews', reviewsController);
-
-// Route for camels
-app.use('/api/camels', camelsController);
 
 
 // Catch all non-error handler for api (i.e., 404 Not Found)
