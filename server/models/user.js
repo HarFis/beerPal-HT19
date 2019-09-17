4dd317e434277509ea64679e6e66d4862842d924
vaara <jonatan.vaara94@gmail.com>
2019-09-17 10:26:45 +0200

    Creates Controllers for user and post
    A bunch of methods haha
    relates to #2

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var posts = require('../models/post');
 
 var userSchema = new Schema({
     username : { type : String, unique : true, required : true, dropDups : true},
-    mail : { type : String},
+    mail : { type : String, match: /.+@.+\..+/},
     password : { type : String},
     // TODO PROFILE PICTURE
-    friends : [String]
+    following : [{ type: mongoose.Schema.Types.ObjectId, 
+        ref: 'users'}]
     
 });
 
