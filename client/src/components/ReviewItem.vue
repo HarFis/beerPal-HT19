328afd36214a1bdeb94a79ab57b201f1564fef8d
HarFis <gusfisha@student.gu.se>
2019-09-24 13:43:46 +0200

    Add details in Review list

--- client/src/components/ReviewItem.vue
+++ client/src/components/ReviewItem.vue
@@ -2,7 +2,8 @@
 <b-list-group>
   <b-list-group-item>
    <!-- <img alt="Review" src="../assets/camel.jpg"> -->
-    {{ review.name }} has a score of {{ review.score }}
+    A BeerPal drunk {{ review.beer.name }} on {{ review.created }}. He/She gave the beer {{ review.score }} points and wrote:
+    {{ review.textReview }}
     <b-button type="button" class="close" @click="$emit('delete-review', review._id)">&times;</b-button>
   </b-list-group-item>
   </b-list-group>
