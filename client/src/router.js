c499dcac9ce3ce2fa48728e6d9891ccc3ef4687a
vaara <jonatan.vaara94@gmail.com>
2019-09-24 13:35:54 +0200

    Tries to fix merge thing

--- client/src/router.js
+++ client/src/router.js
@@ -3,14 +3,11 @@ import Router from 'vue-router'
 
 import Home from './views/Home.vue'
 import Camels from './views/Camels.vue'
-<<<<<<< HEAD
 import Users from './views/Users.vue'
 import LoginPage from './views/Login.vue'
 import RegisterPage from '.views/Register.vue'
-=======
 import Reviews from './views/Reviews.vue'
 import Beers from './views/Beers.vue'
->>>>>>> 8e97af5cec1c3dc2391cb60d6f72d14795d8becf
 
 Vue.use(Router)
 
@@ -29,7 +26,6 @@ export default new Router({
       component: Camels
     },
     {
-<<<<<<< HEAD
       path: '/users',
       name: 'users',
       component: Users
@@ -43,7 +39,8 @@ export default new Router({
       path: '/register',
       name: 'register',
       component: RegisterPage
-=======
+    },
+    {
       path: '/reviews',
       name: 'reviews',
       component: Reviews
@@ -52,7 +49,6 @@ export default new Router({
       path: '/beers',
       name: 'beers',
       component: Beers
->>>>>>> 8e97af5cec1c3dc2391cb60d6f72d14795d8becf
     }
   ]
 })
