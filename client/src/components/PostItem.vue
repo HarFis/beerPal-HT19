0407f1b787ed9ea3d26c2d2f97f249b55d72b3bd
vaara <jonatan.vaara94@gmail.com>
2019-09-27 19:14:27 +0200

    DateTime-fixes install moments --save for it to work Fixes so that we can format date and time
    Formatted date and time for UserItem

--- client/src/components/PostItem.vue
+++ client/src/components/PostItem.vue
@@ -6,8 +6,9 @@
     <b-row>
     <b-col> <p class="text-left">Beer: {{ post.review.beer.name }} </p></b-col>
     <b-col> Score: <span class="badge badge-primary badge-pill">{{ post.review.score }}</span> points </b-col>
-    <b-col> consumed on: {{ post.review.created }} </b-col>
-    </b-row>
+    <b-col> consumed on: <p>{{ changeFormat(this.post.review.created)}}</p>  </b-col>
+    <b-col> at: <p>{{ changeFormat2(this.post.review.created)}}</p>  </b-col>
+    </b-row> 
     <b-row v-show="post.review.textReview">
       <b-col>
       <p class="text-left">BeerPals opinion: <span class="opinion">{{ post.review.textReview }}</span></p>
@@ -19,10 +20,26 @@
 </template>
 
 <script>
+import moment from 'moment'
 
 export default {
   name: 'post-item',
-  props: ['post']
+  props: ['post'],
+
+
+  methods: {
+      changeFormat(value){
+      if (value) {
+           return moment(String(value)).format('YYYY-MM-DD')
+      }
+      },
+  changeFormat2(value){
+      if (value) {
+           return moment(String(value)).format('h:mm:ss a')
+      }
+    }
+  }
+  
 }
 
 </script>
