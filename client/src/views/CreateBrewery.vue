5115693ae3e586ec26d108b257611471c258e86e
strand93 <strand.93@hotmail.com>
2019-09-27 11:47:34 +0200

    Subject: Adds CreateBrewery
    CreateBrewery view, not complete
    
    Related Issue(s):

---
+++ client/src/views/CreateBrewery.vue
@@ -0,0 +1,35 @@
+<template>
+    
+</template>
+
+<script>
+import { Api } from '@/Api'
+
+export default {
+    name: 'CreateBrewery',
+    date() {
+        return{
+            name: null,
+            adress: null,
+            link: null
+        }
+    },
+    methods: {
+        onSubmit() {
+            var brewery = {
+                name: this.name,
+                adress: this.adress,
+                link: this.link
+            }
+            Api.post('/breweries', breweries)
+            .catch(error => {
+                console.log(error)
+            })
+            this.name = null
+            this.adress = null
+            this.link = null
+        }
+    }
+    
+}
+</script>
