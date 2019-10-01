7cca1baadcfb43d6246fc7fe60afa891056b78de
strand93 <strand.93@hotmail.com>
2019-10-01 11:09:48 +0200

    Subject: Edit brewery
    BreweryInfo updates automatic after edit a brewery
    
    Related Issue(s):

--- client/src/components/BreweryItem.vue
+++ client/src/components/BreweryItem.vue
@@ -1,10 +1,12 @@
 <template>
+    <div>
       <b-list-group-item>
       <img alt="Brewery" src="../assets/brewery.png">{{ brewery.name }}, Adress: {{ brewery.adress }}
       , Link: {{ brewery.link}}
       <b-button pill variant="outline-info" size="sm" router-link :to="'/breweries/'  + brewery._id ">more details</b-button>
       <b-button type="button" class="close" @click="$emit('delete-brewery', brewery._id)">&times;</b-button>
       </b-list-group-item>
+    </div>
 </template>
 
 <script>
