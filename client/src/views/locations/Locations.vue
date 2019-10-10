47ad50cd2b53c5a7327aaaf14b048f6f165e8cb4
HarFis <gusfisha@student.gu.se>
2019-10-10 16:12:40 +0200

    Additional message on "server offline"

--- client/src/views/locations/Locations.vue
+++ client/src/views/locations/Locations.vue
@@ -1,28 +1,37 @@
 <template>
-<div>
-  <vue-headful
-            title="Locations - BeerPal"
-        />
-  <div class="locations">
-    <h1>Locations</h1>
-    <h3>Current number of locations: {{ locations.length }}</h3>
-    <b-button class="buttonClass" router-link to="/add-location">Add new location</b-button>
-    <b-list-group>
-      <location-item
-        v-for="location in locations"
-        :key="location._id"
-        :location="location"
-        @delete-location="deleteLocation"
-        @edit-location="editLocation"
-      ></location-item>
-    </b-list-group>
-    <br>
-  <b-button variant="danger" @click="deleteAllLocations" v-show="!(locations.length===0)">Delete all locations</b-button>
-  
+  <div>
+    <vue-headful title="Locations - BeerPal" />
+    <div class="locations">
+      <h1>Locations</h1>
 
-  
+      <span v-if="!serverOK">
+        <h3>
+          <img style="max-width: 40px;" :src="require(`@/assets/warning.png`)" alt="warning" /> No connection to server. Please retry or contact administrator!
+        </h3>
+      </span>
+
+      <span v-else>
+        <h3>Current number of locations: {{ locations.length }}</h3>
+
+        <b-button class="buttonClass" router-link to="/add-location">Add new location</b-button>
+      </span>
+      <b-list-group>
+        <location-item
+          v-for="location in locations"
+          :key="location._id"
+          :location="location"
+          @delete-location="deleteLocation"
+          @edit-location="editLocation"
+        ></location-item>
+      </b-list-group>
+      <br />
+      <b-button
+        variant="danger"
+        @click="deleteAllLocations"
+        v-show="!(locations.length===0)"
+      >Delete all locations</b-button>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -33,6 +42,7 @@ export default {
   name: "Locations",
   data() {
     return {
+      serverOK: true,
       show: false,
       locations: []
     };
@@ -42,11 +52,13 @@ export default {
   },
   methods: {
     getLocations() {
+      this.serverOK = true;
       Api.get("/locations")
         .then(response => {
           this.locations = response.data.locations;
         })
         .catch(error => {
+          this.serverOK = false;
           this.locations = [];
           console.log(error);
         })
@@ -70,24 +82,23 @@ export default {
       Api.patch(`/locations/${id}`, newLocation)
         .then(response => {
           console.log(response.data);
-            this.getLocations();
+          this.getLocations();
         })
         .catch(error => {
           console.log(error);
         });
     },
     deleteAllLocations() {
-        if(confirm('Are you sure you want to delete all locations?')){
-        Api.delete('locations')
-        .then(response =>{
-          this.locations = []
-        })
-        .catch(error =>{
-          console.log(error)
-        })
+      if (confirm("Are you sure you want to delete all locations?")) {
+        Api.delete("locations")
+          .then(response => {
+            this.locations = [];
+          })
+          .catch(error => {
+            console.log(error);
+          });
       }
     }
-
   },
 
   components: {
@@ -98,7 +109,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 .createButton {
   margin-bottom: 1em;
 }
