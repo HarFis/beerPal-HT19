40b0e93e77a3568192ae9ad9c9e018dfb956cf46
HarFis <gusfisha@student.gu.se>
2019-10-21 14:53:58 +0200

    fix thing

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -9,49 +9,49 @@ var Post = require('../models/post');
 -----FUNCTION---
 ---------------*/
 //Function for setting the average score to a beer when creating/updating/deleting a related review
-function setAverageScore( id, req, res, next){
-    Review.find({beer : id}).exec(function (err, reviews){
+function setAverageScore(id, req, res, next) {
+    Review.find({ beer: id }).exec(function (err, reviews) {
         if (err) { return next(err); }
         if (!reviews.length) {
             Beer.findById({ _id: id }, function (err, beer) {
                 if (err) { return next(err); }
                 beer.averageRating = null;
                 beer.save();
-                
+
             });
             return //res.status(404).json({'message': 'Beer reviews not found'});
         }
         var totalScores = 0;
-        for(var i = 0; i < reviews.length; i++){
+        for (var i = 0; i < reviews.length; i++) {
             totalScores += reviews[i].score;
         }
-        var avgScore = (totalScores/reviews.length);
-        var avgScore = Math.round(avgScore*10)/10;
-        
+        var avgScore = (totalScores / reviews.length);
+        var avgScore = Math.round(avgScore * 10) / 10;
+
         Beer.findById({ _id: id }, function (err, beer) {
             if (err) { return next(err); }
             beer.averageRating = avgScore;
             beer.save();
-            
+
         });
         //res.status(201).json({message: "Average score updated"});
-        
+
     });
- }
+}
 
 /*------------
 -----POST-----
 ------------*/
 
 // Create a new review & update avg. score 
-router.post('/', function (req, res, next){
+router.post('/', function (req, res, next) {
     var beer_id = req.body.beerID;
-    if( !mongoose.Types.ObjectId.isValid(beer_id) ){
-        return res.status(404).json({message: "Beer not found in DB"}); // They didn't send an object ID
-      }
-    Beer.findById(beer_id, function (err, beer){
+    if (!mongoose.Types.ObjectId.isValid(beer_id)) {
+        return res.status(404).json({ message: "Beer not found in DB" }); // They didn't send an object ID
+    }
+    Beer.findById(beer_id, function (err, beer) {
         if (err) { return next(err); }
-        
+
         if (!beer) {
             return res.status(404).json({
                 message: "Beer not found in DB"
@@ -63,7 +63,7 @@ router.post('/', function (req, res, next){
             textReview: req.body.textReview,
             created: new Date(),
         });
-        review.save(function(err){
+        review.save(function (err) {
             if (err) { return next(err); }
             setAverageScore(beer_id, req, res, next);
             res.status(201).json(review);
@@ -94,17 +94,17 @@ router.get('/', function (req, res, next) {
 // not used in Frontend/Android
 router.get('/:id', function (req, res, next) {
     var id = req.params.id;
-    if( !mongoose.Types.ObjectId.isValid(id) ){
-        return res.status(404).json({message: "Review not found"});
-      }
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ message: "Review not found" });
+    }
     Review.findById(id)
-        .populate({path: 'beer', populate: { path: 'brewery' }})
+        .populate({ path: 'beer', populate: { path: 'brewery' } })
         .exec()
         .then(review => {
             if (!review) {
                 return res.status(404).json({ message: "Review not found" });
             }
-            res.status(200).json({review });
+            res.status(200).json({ review });
         })
         .catch(err => {
             res.status(500).json({
@@ -120,9 +120,9 @@ router.get('/:id', function (req, res, next) {
 // Update (partially) a review with the given ID & update avg. score
 router.patch('/:id', function (req, res, next) {
     var id = req.params.id;
-    if( !mongoose.Types.ObjectId.isValid(id) ){
-        return res.status(404).json({message: "Review not found"}); 
-      }
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ message: "Review not found" });
+    }
     Review.findById({ _id: id }, function (err, review) {
         if (err) { return next(err); }
         if (review === null) {
@@ -132,14 +132,14 @@ router.patch('/:id', function (req, res, next) {
         review.textReview = (req.body.textReview || review.textReview);
         review.beer = (req.body.beer || review.beer);
         review.created = (req.body.created || review.created);
-        review.save(function(err, review) {
+        review.save(function (err, review) {
             review
-            .populate('beer')
-            .execPopulate()
+                .populate('beer')
+                .execPopulate()
             var beerId = review.beer;
-            setAverageScore(beerId, req, res, next);  
+            setAverageScore(beerId, req, res, next);
         });
-        res.json({review});
+        res.json({ review });
     });
 });
 
@@ -152,12 +152,13 @@ router.patch('/', function (req, res, next) {
         if (reviews === null) {
             return res.status(404).json({ 'message': 'Review not found' });
         }
-        for(var i = 0; i>reviews.length; i++)
-        {review[i].score = (req.body.score|| review.score);
-        review[i].textReview = (req.body.textReview|| review.textReview);
-        review[i].beer = (req.body.beer|| review.beer);
-        review[i].created = (req.body.created|| review.created);
-        review[i].save();}
+        for (var i = 0; i > reviews.length; i++) {
+            review[i].score = (req.body.score || review.score);
+            review[i].textReview = (req.body.textReview || review.textReview);
+            review[i].beer = (req.body.beer || review.beer);
+            review[i].created = (req.body.created || review.created);
+            review[i].save();
+        }
         res.json(review);
     });
 });
@@ -170,11 +171,11 @@ router.patch('/', function (req, res, next) {
 // Delete the review with the given ID & the related post & update avg. score
 router.delete('/:id', function (req, res, next) {
     var id = req.params.id;
-    if( !mongoose.Types.ObjectId.isValid(id) ){
-        return res.status(404).json({message: "Review not found"}); // They didn't send an object ID
-      }
-    Post.findOneAndDelete({review: id}, function(err){
-        if (err) { return next(err); } 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ message: "Review not found" }); // They didn't send an object ID
+    }
+    Post.findOneAndDelete({ review: id }, function (err) {
+        if (err) { return next(err); }
     });
 
     Review.findById({ _id: id }, function (err, review) {
@@ -184,27 +185,28 @@ router.delete('/:id', function (req, res, next) {
         }
         var beerId = review.beer;
         console.log(beerId)
-        setAverageScore(beerId, req, res, next);
         review.delete();
-        review.save();   
-        res.status(200).json(review);
-
+        review.save(function (err) {
+            if (err) { return next(err); }
+            setAverageScore(beerId, req, res, next);
+            res.status(200).json(review);
+        });
     });
-    
 });
 
 // Delete all reviews & removes all posts & sets avg. rating to 0 for all beers (since no reviews exist)
 router.delete('/', function (req, res, next) {
-    Review.find().deleteMany().exec(function(err, reviews) {
+    Review.find().deleteMany().exec(function (err, reviews) {
         if (err) { return next(err); }
     });
-    Post.find().deleteMany().exec(function(err) {
+    Post.find().deleteMany().exec(function (err) {
         if (err) { return next(err); }
     });
-    Beer.find(function(err, beers){
-        if (err) { return next(err); 
-        } 
-        for(var i = 0; i < beers.length; i ++){
+    Beer.find(function (err, beers) {
+        if (err) {
+            return next(err);
+        }
+        for (var i = 0; i < beers.length; i++) {
             console.log(beers[i].averageRating)
             beers[i].averageRating = null;
             beers[i].save();
